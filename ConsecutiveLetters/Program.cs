6aa7a72a4f30cc3af439d5c4ce8f828ebe26e022
adelinegrama <grama.alina2003@gmail.com>
2024-09-12 21:56:35 +0300

    first commit

---
+++ ConsecutiveLetters/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ConsecutiveLetters
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Task 71
+            //check if a given string contains two similar consecutive letters
+
+            Console.WriteLine("Enter a string that may or may not contain consecutive letters:");
+           // string? userInput = Console.ReadLine();
+
+            bool UserInput(string? userInput)
+            {
+                for (int i = 1; i < userInput?.Length; i++)
+                {
+                    if (userInput[i] == userInput[i - 1])
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+
+            var result = UserInput(Console.ReadLine());
+            Console.WriteLine(result);
+        }
+    }
+}
